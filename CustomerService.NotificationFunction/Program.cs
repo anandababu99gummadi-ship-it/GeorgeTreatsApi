64b44a750687eabb44eec3ad2c512df6033c69ec
AnanJ <anandababu99.gummadi@gmail.com>
2026-08-05 12:16:02 +0530

    Commit message: Add Key Vault integration to NotificationFunction for production deployment

--- CustomerService.NotificationFunction/Program.cs
+++ CustomerService.NotificationFunction/Program.cs
@@ -1,7 +1,9 @@
+using Azure.Identity;
 using Azure.Monitor.OpenTelemetry.Exporter;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Azure.Functions.Worker.OpenTelemetry;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using OpenTelemetry;
@@ -9,6 +11,17 @@ using OpenTelemetry;
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
+
+// Key Vault configuration adding
+var keyVaultUrl = new Uri("https://georgetreats-keyvault.vault.azure.net/");
+var credential = builder.Environment.IsDevelopment()
+    ? new DefaultAzureCredential(new DefaultAzureCredentialOptions
+    {
+        ExcludeManagedIdentityCredential = true
+    })
+    : new DefaultAzureCredential();
+
+builder.Configuration.AddAzureKeyVault(keyVaultUrl, credential);
 
 if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING")))
 {
